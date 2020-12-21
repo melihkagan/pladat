ced92c4f1ce7a85f91d6debb200a055f6723ee90
teloglu17@itu.edu.tr <mfteloglu@gmail.com>
2020-12-21 13:56:32 +0300

    database modified

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,52 +1,74 @@
 from django.db import models
 
 class Employer(models.Model):
-    username = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    company = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
-
-class Student(models.Model):
-    username = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    university = models.CharField(max_length=100)
-    skill_1 = models.CharField(max_length=100,default="")
-    condition_1 = models.IntegerField(default=0)
-    skill_2 = models.CharField(max_length=100,default="")
-    condition_2 = models.IntegerField(default=0)
-    skill_3 = models.CharField(max_length=100,default="")
-    condition_3 = models.IntegerField(default=0)
-    skill_4 = models.CharField(max_length=100,default="")
-    condition_4 = models.IntegerField(default=0)
-    skill_5 = models.CharField(max_length=100,default="")
-    condition_5 = models.IntegerField(default=0)
+    user_id = models.IntegerField(default=None)
+    e_mail = models.EmailField(default=None)
+    name = models.CharField(max_length=100,default=None)
+    surname = models.CharField(max_length=100,default=None)
+    company_name = models.CharField(max_length=100,default=None,null=True)
+    sector = models.CharField(max_length=100,default=None,null=True)
+    phone = models.CharField(max_length=13,default=None,null=True)
+    website = models.CharField(default="",max_length=100,null=True)
+    address = models.CharField(default="",max_length=100,null=True)
 
     def __str__(self):
-        return self.skill_1
+        return self.name
+
+class Student(models.Model):
+    user_id = models.IntegerField(default=None,null=True)
+    name = models.CharField(max_length=100,default=None)
+    surname = models.CharField(max_length=100,default=None)
+    school_name = models.CharField(max_length=100,default=None,null=True)
+    department = models.CharField(max_length=100,default=None,null=True)
+    birth_date = models.DateField(null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(default=None,null=True)
+    cgpa = models.FloatField(default=0.0,null=True) #gpa
+    e_mail = models.EmailField(default=None,null=True)
+
+    def __str__(self):
+        return self.name
 
 class Job(models.Model):
-    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
-    job_title = models.CharField(max_length=100)
-    job_description = models.CharField(max_length=100)
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE,default=None) #employer_id
+    job_title = models.CharField(max_length=100,default=None)
+    job_description = models.CharField(max_length=100,default=None) #description
     publish_time = models.DateTimeField(auto_now_add=True, blank=True)
+    due_date = models.DateField(default=None,null=True)
+    req_departments = models.CharField(max_length=100,default=None,null=True)
 
-    skill_1 = models.CharField(max_length=100,default="")
-    condition_1 = models.IntegerField(default=0)
-    skill_2 = models.CharField(max_length=100,default="")
-    condition_2 = models.IntegerField(default=0)
-    skill_3 = models.CharField(max_length=100,default="")
-    condition_3 = models.IntegerField(default=0)
-    skill_4 = models.CharField(max_length=100,default="")
-    condition_4 = models.IntegerField(default=0)
-    skill_5 = models.CharField(max_length=100,default="")
-    condition_5 = models.IntegerField(default=0)    
+    def __str__(self):
+        return self.job_title
+
 class Application(models.Model):
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    username = models.CharField(max_length=100)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE,default=None)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE,default=None)
     apply_time = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.apply_time 
+
 class Skill(models.Model):
-    skill = models.CharField(max_length=100)
+    skill = models.CharField(max_length=100,default=None)
+
+    def __str__(self):
+        return self.skill
+
+class JobSkill(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE,default=None)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE,default=None)
+    rate = models.IntegerField(default=0)    
+
+class StudentSkill(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE,default=None)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE,default=None)
+    rate = models.IntegerField(default=0)
+
+class Setting(models.Model):
+    not_news = models.BooleanField(default=False)
+    not_messages = models.BooleanField(default=False)
+    not_matches = models.BooleanField(default=False)
+
+
 
 # Create your models here.
